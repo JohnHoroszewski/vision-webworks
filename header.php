77b85ee24338fdda201aee9c3d8cd6077d57c0ea
John Horoszewski <johnhoroszewski@johns-mbp.myfiosgateway.com>
2021-01-17 15:47:01 -0500

    Added new styling and css animations to Services page items

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta >
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<style>.home-slider, .testimonial-slider { display: none; }</style>
